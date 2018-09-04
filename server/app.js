4fffdb3f0d2929ec9938fc3fe45377716b551800
Stanley Huang <infamous_godhand@yahoo.com>
2018-09-04 12:10:26 +0000

    set up schema

--- server/app.js
+++ server/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
 const app = express();
+const schema = require("./schema/schema.js");
 
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
 app.listen(process.env.PORT, () => console.log("server running"));

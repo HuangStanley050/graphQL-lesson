5c22f30fc1c7e8746b6d88bbb7dc5495e289c232
Stanley Huang <infamous_godhand@yahoo.com>
2018-09-12 09:23:30 +0000

    able to fetch authors from database to select

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import BookList from './components/booklist';
+import AddBook from './components/addbook';
 
 //apollo client
 const client = new ApolloClient({
@@ -15,6 +16,7 @@ class App extends Component {
           <div id="main">
               <h1>Ninja Book Reading List</h1>
               <BookList/>
+              <AddBook/>
           </div>
         </ApolloProvider>
     );
